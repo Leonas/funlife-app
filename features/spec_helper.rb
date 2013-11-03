ab03e5df7d2dbca792fa28acb3479835a3f57e94
Leonas Liudzius <leonasl@gmail.com>
2013-11-02 18:52:40 -0700

    removed unnecessary things

--- features/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-#this is all wrong
-
-require 'billy/cucumber'
-
-Capybara.javascript_driver = :poltergeist_billy
-
--- features/support/env.rb
+++
@@ -1,3 +0,0 @@
-require 'rubygems'
-require 'capybara'
-require 'capybara/cucumber'
